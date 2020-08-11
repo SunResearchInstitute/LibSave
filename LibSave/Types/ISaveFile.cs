2fb3b10fd1525b613ec050730c50c2dc1bd65d05
Sun <gabehttps@gmail.com>
2020-08-11 10:09:03 -0700

    Controller

---
+++ LibSave/Types/ISaveFile.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace LibSave.Types
+{
+    public abstract class ISaveFile
+    {
+        public FileInfo FilePath;
+
+        protected ISaveFile(FileInfo filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public abstract void Write();
+
+        public abstract Task WriteAsync();
+
+        public abstract void Reload();
+    }
+}
---
+++ LibSave/Types/SaveFile.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LibSave.Types
+{
+    public abstract class SaveFile<T> : ISaveFile where T : new()
+    {
+        protected T Data;
+
+        protected SaveFile(FileInfo filePath, T defaultData) : base(filePath)
+        {
+            LoadData(defaultData);
+        }
+
+        public override void Write() => FilePath.WriteAllText(JsonConvert.SerializeObject(Data, Formatting.Indented));
+
+        public override Task WriteAsync() => Task.Run(() => Write());
+
+        public override void Reload() => Reload(new T());
+
+        public void Reload(T data) => LoadData(data);
+
+        private void LoadData(T fallbackData)
+        {
+            if (FilePath.Exists)
+            {
+                try
+                {
+                    Data = JsonConvert.DeserializeObject<T>(FilePath.ReadAllText());
+                }
+                catch
+                {
+                    if (fallbackData == null)
+                    {
+                        Data = new T();
+                    }
+                    else
+                    {
+                        Data = fallbackData;
+                    }
+                }
+            }
+        }
+    }
+}
